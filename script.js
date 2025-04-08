8f189b61f7f9cfb6ec5cdfe6526b17136563bf99
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-04-08 16:27:31 +0530

    functionalities are also added

---
+++ script.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('#generate');
+const box = document.querySelector('.box');
+
+btn.addEventListener('click', () => {
+    const R = Math.floor(Math.random() * 256)
+    const G = Math.floor(Math.random() * 256)
+    const B = Math.floor(Math.random() * 256)
+    box.style.backgroundColor  = `rgb(${R}, ${G}, ${B})`
+})
